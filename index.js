e485c9b0cf3f146725186aca5cd308fc77f23756
Gavin King <gavin.king@itslearning.com>
2018-02-18 21:24:25 +0100

    autocompletion

--- index.js
+++ index.js
@@ -2,26 +2,48 @@
 
 /** @type {HTMLTextAreaElement} */
 const target = document.querySelector('#code-editor-mount');
+
 /** @type {HTMLElement} */
 const output = document.querySelector('#output');
 
+/** @type {string} */
+// Default content to render.
+target.value = `<style>
+.jeff {
+     color: red;
+}
+</style>
+
+<h1 class="jeff">A header</h1>`;
+
 /** @type {CodeMirror.EditorConfiguration} */
 const codeMirrorConfig = {
-    value: target.value + '\n',
+    extraKeys: {'Ctrl-Space': 'autocomplete'},
     lineNumbers: true,
-    mode:  'htmlmixed'
+    mode:  'text/html',
+    value: target.value
 };
 
 /** @type {CodeMirror.Editor} */
 const codeMirror = CodeMirror(mountReplace(target), codeMirrorConfig);
 
-codeMirror.on('change', editor => {
-    output.innerHTML = editor.doc.getValue();
-});
+codeMirror.on('change', onChange(output));
+
+render(output)(codeMirror);
 
 // /////////////////////////////////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////////////////////////
+
+function onChange(output) {
+    return _.throttle(render(output), 500);
+}
+
+function render(output) {
+    return editor => {
+        output.innerHTML = editor.doc.getValue();
+    };
+}
 
 /**
  * Removes original mount element from DOM.
@@ -34,7 +56,7 @@ function mountReplace(target) {
      * @param {HTMLElement} elt
      * @returns {void}
      */
-    return function (elt) {
+    return elt => {
         target.parentNode.replaceChild(elt, target);
     };
 }
