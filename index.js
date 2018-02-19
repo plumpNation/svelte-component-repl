34fdd54db191c7cd7e9e7fc1eb001762f0c57a48
Gavin King <gavin.king@itslearning.com>
2018-02-19 01:03:43 +0100

    rendering svelte component

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 // @ts-check
 
+const store = {};
+
 /** @type {HTMLTextAreaElement} */
 const target = document.querySelector('#code-editor-mount');
 
 /** @type {HTMLElement} */
 const output = document.querySelector('#output');
 
-const customComponentNames = [
-    '<MyComponent',
-    '<YourComponent'
-];
+const components = [
+    {
+        name: 'MyComponent',
+        type: 'svelte',
+        source: '<h1>I am a svelte MyComponent</h1>'
+    },
+    {
+        name: 'YourComponent',
+        type: 'svelte',
+        source: '<h1>I am a svelte YourComponent</h1>'
+    }
+].map(component => {
+    component.compiled = compile(component);
+
+    return component;
+});
 
 /** @type {string} */
 // Default content to render.
 target.value = `<style>
-.jeff {
+.foobar,
+MyComponent h1 {
      color: red;
 }
 </style>
 
-<h1 class="jeff">A header</h1>`;
+<h1 class="foobar">Dummy data</h1>
+
+<MyComponent></MyComponent>
+`;
 
 /** @type {CodeMirror.EditorConfiguration} */
 const codeMirrorConfig = {
@@ -38,8 +56,9 @@ const originalHTMLHint = CodeMirror.hint.html;
 // HTML autocomplete list.
 CodeMirror.hint.html = function(cm) {
     const inner = originalHTMLHint(cm) || {from: cm.getCursor(), to: cm.getCursor(), list: []};
+    const componentNames = components.map(customComponent => '<' + customComponent.name);
 
-    inner.list.unshift.apply(inner.list, customComponentNames);
+    inner.list.unshift.apply(inner.list, componentNames);
 
     return inner;
 };
@@ -51,9 +70,34 @@ codeMirror.on('change', onChange(output));
 
 render(output)(codeMirror);
 
+updateBundle(components);
+
 // /////////////////////////////////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////////////////////////
+
+function compile(component) {
+    const warnings = [];
+
+    if (component.type === 'js') {
+        return {
+            code: component.source,
+            map: null,
+            warnings
+        };
+    }
+
+    const { code, map } = svelte.compile(component.source || '', {
+        cascade: false,
+        name: component.name,
+        filename: component.name + '.html',
+        onwarn: warning => {
+            warnings.push(warning);
+        }
+    });
+
+    return { code, map, warnings };
+}
 
 function onChange(output) {
     return _.throttle(render(output), 500);
@@ -61,7 +105,28 @@ function onChange(output) {
 
 function render(output) {
     return editor => {
-        output.innerHTML = editor.doc.getValue();
+        let markup = '';
+
+        if (store.bundle) {
+            markup += `<script>
+                ${store.bundle.code}
+
+                store.target = document.querySelector('MyComponent');
+
+                new MyComponent({target: store.target});
+            </script>`;
+        }
+
+        markup += editor.doc.getValue();
+
+        // not great
+        output.innerHTML = markup;
+
+        const scriptTags = output.getElementsByTagName('script');
+
+        [].forEach.call(scriptTags, scriptTag => {
+            eval(scriptTag.text);
+        });
     };
 }
 
@@ -78,5 +143,118 @@ function mountReplace(target) {
      */
     return elt => {
         target.parentNode.replaceChild(elt, target);
+    };
+}
+
+function updateBundle(components) {
+    if (!components || !components.length) return;
+    if (components.some(c => !c.compiled)) return;
+
+    // console.clear();
+    console.log(`running Svelte compiler version %c${svelte.VERSION}`, 'font-weight: bold');
+
+    if (window.bundlePromise) window.bundlePromise.cancel();
+
+    const lookup = {};
+    let warningCount = 0;
+
+    components.forEach(component => {
+        const w = component.compiled.warnings.length;
+
+        warningCount += w;
+
+        if (w > 0) {
+            console.group(`${component.name}.${component.type}: ${w} ${w === 1 ? 'warning' : 'warnings'}`);
+
+            component.compiled.warnings.forEach(warning => {
+                console.warn(warning.message);
+                console.log(warning.frame);
+            });
+
+            console.groupEnd();
+        }
+
+        const path = `./${component.name}.${component.type}`;
+
+        if (path in lookup) {
+            throw new TypeError(`Multiple ${component.name}.${component.type} components`);
+        }
+
+        lookup[path] = {
+            code: component.compiled.code,
+            map: component.compiled.map
+        };
+    });
+
+    console.warn('warningCount', warningCount);
+
+    let cancelled = false;
+
+    let uid = 1;
+    const importMap = new Map();
+
+    // here we can move between different entry points
+    const input = './MyComponent.svelte';
+
+    window.bundlePromise = rollup.rollup({
+        input,
+        external: id => {
+            return id[0] !== '.';
+        },
+        plugins: [{
+            resolveId(importee, importer) {
+                if (importee[0] === '.') return importee;
+            },
+            load(id) {
+                if (id in lookup) return lookup[id];
+
+                if (id[0] === '.') {
+                    throw new Error(`file does not exist`);
+                }
+
+                return null;
+            }
+        }],
+        onwarn(warning) {
+            if (warning.code === 'MISSING_GLOBAL_NAME') return;
+
+            console.warn(warning.message);
+        }
+    })
+    .then(bundle => {
+        if (cancelled) return;
+
+        return bundle.generate({
+            format: 'iife',
+            name: 'MyComponent',
+            globals: id => {
+                const name = `import_${uid++}`;
+                importMap.set(id, name);
+                return name;
+            },
+            sourcemap: true
+        })
+        .then(({ code, map }) => {
+            store.bundle = {
+                code,
+                map,
+                imports: bundle.imports,
+                importMap
+            };
+
+            store.bundleError = null;
+            store.runtimeError = null;
+
+            render(output)(codeMirror);
+        });
+    })
+    .catch(err => {
+        console.error(err.stack);
+
+        store.bundleError = err;
+    });
+
+    window.bundlePromise.cancel = () => {
+        cancelled = true;
     };
 }
